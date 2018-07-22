9055b122bb73aeec7c9fb153398870446827ace8
Dmitry Dubchak <neighborland@outlook.com>
2018-07-22 19:52:40 +0700

    bootstrap formhelpers included

--- jscripts/main-w-jquery.js
+++ jscripts/main-w-jquery.js
@@ -1,48 +1,25 @@
 $(document).ready(function () {
-    var addContactButton = $("#add-contact-button");
-    var lastName = $("#last-name-input");
-    var firstName = $("#first-name-input");
-    var phone = $("#phone-input");
-    var contactList = $("#contact-list");
+    $(document).on('click', '.plus', function () {
+        // получаем поля из формы ввода
+        var lastName = $('#last-name-input').val();
+        var firstName = $('#first-name-input').val();
+        var phone = $('#phone-input').val();
 
-    addContactButton.click(function () {
-        var lastNameValue = lastName.val();
-        var firstNameValue = firstName.val();
-        var phoneValue = phone.val();
-        var contactListItem = $("<li>");
-
-        var taskDelButton = $("<button>")
-        // .css({"margin-left": "30px"})
-            .text("Удалить")
-            .addClass("del-contact-button")
-            .click(function () {
-                contactListItem.remove();
-            });
-
-        contactListItem.text(lastNameValue + " " + firstNameValue + " " + phoneValue);
-        contactListItem.append(taskDelButton);
-        contactList.append(contactListItem);
-        firstName.val("");
-        lastName.val("");
-        phone.val("");
-    });
-// формируем новые поля\
-    var number = 1;
-    jQuery('.plus').click(function () {
-        jQuery('.contact_json_plus').before(
+        $('.table tr:last').after(
             '<tr>' +
-            '<td><input type="text" class="form-control" id="contact_json_number[]" placeholder=' + number + '></td>' +
-            '<td><input type="text" class="form-control" id="contact_json_last_name[]" placeholder="Фамилия"></td>' +
-            '<td><input type="text" class="form-control" id="contact_json_first_name[]" placeholder="Имя"></td>' +
-            '<td><input type="tel" class="form-control bfh-phone" data-format="+7 (ddd) ddd-dddd"' +
-            ' pattern="(?:\\+|\\d)[\\d\\-\\(\\) ]{9,}\\d/g" id="contact_json_phone[]" placeholder="Телефон"></td>' +
-            '<td><span class="btn btn-danger minus pull-right">x</span></td>' +
+            '<td><input type="text" class="form-control row-index" value="' + $('.table tr').length + '"/></td>' +
+            '<td><input type="text" class="form-control" value="' + lastName + '"/></td>' +
+            '<td><input type="text" class="form-control" value="' + firstName + '"/></td>' +
+            '<td><input type="text" class="form-control bfh-phone" data-format="+7 (ddd) ddd-dddd" value="' + phone + '"/></td>' +
+            '<td><button type="button" class="btn btn-danger minus pull-right">Удалить</button></td>' +
             '</tr>'
         );
-        number++;
+
     });
-// on - так как элемент динамически создан и обычный обработчик с ним не работает
-    jQuery(document).on('click', '.minus', function () {
-        jQuery(this).closest('tr').remove(); // удаление строки с полями
+    $(document).on('click', '.minus', function () {
+        $(this).closest('tr').remove();
+        $('.row-index').each(function (i) {
+            $(this).val(i + 1);
+        });
     });
 });
